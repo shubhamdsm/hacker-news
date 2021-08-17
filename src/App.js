9ccc090f11b4f7defbe69a37fa9387e85ffedbc4
shubham <shubhamdv19@gmail.com>
2021-08-17 14:30:10 +0530

    added font

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import { Box } from '@chakra-ui/react'
 function App() {
   return (
     <div className="App">
-     <h1>hello</h1>
+      <h2>hello</h2>
     </div>
   );
 }
